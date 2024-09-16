155640fdc97b31563f6c6967d3d4cd654e07c358
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-09-17 00:54:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import "./App.css"
-import Error from './../../02_Second/src/React-Router/Error';
+
 const App = () => {
 
   const [input, setInput]=useState('5444');
